183ed131edf870ae919386febd97b98d0439efba
Bassem <bassem@revenuemonster.my>
2021-12-11 10:59:35 +0800

    fix: added version tag to branch

--- api.go
+++ api.go
@@ -2,8 +2,9 @@ package sdk
 
 // Error :
 type Error struct {
-	Code        string      `json:"code"`
-	Message     string      `json:"message"`
+	Code    string `json:"code"`
+	Message string `json:"message"`
+
 	Debug       string      `json:"debug"`
 	Description interface{} `json:"description"`
 }
