757416862589223c14f119064e12e12aab570a33
Bassem <bassem@revenuemonster.my>
2021-12-11 11:03:38 +0800

    fix: testing with version tag

--- api.go
+++ api.go
@@ -2,9 +2,8 @@ package sdk
 
 // Error :
 type Error struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-
+	Code        string      `json:"code"`
+	Message     string      `json:"message"`
 	Debug       string      `json:"debug"`
 	Description interface{} `json:"description"`
 }
